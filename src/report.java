66a10b787c2da5c01f8c28647b01c0f563a0da8d
ryunosuke higa <e235711@ie.u-ryukyu.ac.jp>
2023-12-11 17:10:12 +0900

    step1

--- src/report.java
+++ src/report.java
@@ -1,12 +1,8 @@
 public class  report{
  public static void main(String[] args) {
-    try {
+   
         String str = null;
          System.out.println(str.length());
-        } catch (NullPointerException e) {
-             System.out.println("NullPointerExceptionが発生しました。");
-             System.out.println( e.getMessage());
-
+        
+        }
     }
-}
-}
