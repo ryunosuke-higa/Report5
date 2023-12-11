026040fb8bbc6618194734da0299863503cfebe9
ryunosuke higa <e235711@ie.u-ryukyu.ac.jp>
2023-12-11 17:17:12 +0900

    step3

--- src/report.java
+++ src/report.java
@@ -1,8 +1,13 @@
 public class  report{
  public static void main(String[] args) {
-   
-        String str = null;
-         System.out.println(str.length());
+     String str = "壱百満";
+    try {
+       
+        int value = Integer.parseInt(str);
         
+        System.out.println("変換後の値: " + value);
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
         }
     }
+}
